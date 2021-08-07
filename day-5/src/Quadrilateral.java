c3df32026b8b8934ac01bbedb8cdc62296bd6468
saiprathyusha <saiprathyusha9121.com>
2021-08-07 20:02:30 +0530

    third commit

---
+++ day-5/src/Quadrilateral.java
@@ -0,0 +1,45 @@
+
+
+import java.util.Scanner;
+
+public class Quadrilateral extends Area {
+
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println(" R for Rectangle\n S for Sqaure\n T for Trapezium\n Enter the value:");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		if(s.equalsIgnoreCase("R"))
+		{
+			System.out.println("length of rectangle");
+			double l=sc.nextDouble();
+			System.out.println("Breadth of rectangle");
+			double b=sc.nextDouble();
+			System.out.println("Area of rectangle is:");
+			System.out.println(areaOfQuadilateral(l,b));
+		}
+		if(s.equalsIgnoreCase("S"))
+		{
+			System.out.println("Side of Square");
+			double l=sc.nextDouble();
+			System.out.println("Area of square is:");
+			System.out.println(areaOfQuadilateral(l));
+		}
+		if(s.equalsIgnoreCase("T"))
+		{
+			System.out.println("length of one parallel side");
+			double l=sc.nextDouble();
+			System.out.println("length of another parallel side");
+			double b=sc.nextDouble();
+			System.out.println("Height of trapezium");
+			double h=sc.nextDouble();
+			System.out.println("Area of trapezium is:");
+			System.out.println(areaOfQuadilateral(l,b,h));
+		}
+		sc.close(); 
+		
+	
+}
+}
